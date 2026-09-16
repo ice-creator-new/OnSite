5ac9ad70565532547a5f0dd6b7b688202226713c
ice <sc3339354363@outlook.com>
2026-09-16 14:58:35 +0800

    iOS-style glass tab bar and edge-to-edge letters list

--- app/src/main/java/com/suncheng/onsite/MainActivity.kt
+++ app/src/main/java/com/suncheng/onsite/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.suncheng.onsite
 
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
@@ -24,7 +26,10 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.dark(Color.TRANSPARENT),
+            navigationBarStyle = SystemBarStyle.dark(Color.TRANSPARENT),
+        )
         WindowCompat.setDecorFitsSystemWindows(window, false)
         openNoteId = intent.getStringExtra(ArriveNotifier.EXTRA_NOTE_ID)
         setContent {
